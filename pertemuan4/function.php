678c3f6e7693f6f261bb2578fadd5e346151a4bf
AnnasNurC <annasnur10@gmail.com>
2021-03-03 21:13:52 +0700

    mengerjakan tugas pertemuan 4

---
+++ pertemuan4/function.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+    Annas Nur Cholifah
+    203040168
+    https://github.com/AnnasNurC/pw2021_203040168
+    pertemuan 4 == 3 Maret 2021
+    function 
+    */
+
+// membuat function sediri 
+// bisa di isi parameter defaul
+function salam($waktu="datang",$nama= "Admin"){
+    return "Selamat $waktu,$nama";
+};
+
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+    <!-- memenaggil php  -->
+    <!-- parameter harus di isi semuanya  -->
+    <!-- jika parameter tak di isi maka parameter default akan disisi  -->
+    <?= salam(); ?> 
+
+    </h1>
+
+</body>
+</html>
