fd7d1a662414011f5688084695463171ca87f811
AnnasNurC <annasnur10@gmail.com>
2021-03-03 13:37:09 +0700

    mengerjakan pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Annas Nur Cholifah
+203040168
+pertemuan 2 == 23 februari 2021
+mempelajari dasar php
+*/
+
+?>
+
+<?php
+
+
+// Standar output
+// 1. echo ,print
+// 2. print_r
+// 3. var_dump
+
+
+echo"annasnc";
+print"joni";
+print_r("maman");
+var_dump("john");
+
+
+// penulisan sintaxt php
+// 1. php di dalam html
+// 2. html di dalam php
+
+
+
+// variabel dan tipe data
+// variabel
+// tidak boleh di awali dengan angka
+
+// $nama = "annas";
+// echo "hallo nama saya $nama";
+
+
+
+// operator
+// 1. aritmatika
+// + - / % *
+
+// 2. penggabung string / concanitation
+// menggunakan (.) titik
+// $nama_depan = "baban";
+// $nama_belakang = "santoso";
+
+// echo $nama_depan . " " . $nama_belakang ;
+
+// 3. assigment (penugasan)
+// =
+// +=
+//-=
+// /=
+// %=
+// *=
+// .=
+
+// 4. perbandingan
+// > <  >= <= ==   !=
+
+
+// 5. identitas
+// ===  !==
+
+// 6. logika
+// (&&) dan
+// (||) or
+// (!) not
+
+
+
+?>
