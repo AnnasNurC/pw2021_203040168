b76f7a7ac84d87f32f028d97aec3f63b16ac40d0
AnnasNurC <annasnur10@gmail.com>
2021-03-21 20:45:26 +0700

    mengerjakan tugas pertemuan 7

---
+++ pertemuan7/latihan4.php
@@ -0,0 +1,23 @@
+<!-- /*
+Annas Nur Cholifah
+203040168
+https://github.com/AnnasNurC/pw2021_203040168
+pertemuan 7 == 21 Maret 2021
+Get & Post
+*/ -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST</title>
+</head>
+<body>
+    
+    <!-- memanggil data yang dikirim dari latihan 3  -->
+    <h1>Selamat datang <?= $_POST["username"]; ?> </h1>
+
+</body>
+</html>
---
+++ pertemuan7/login/admin.php
@@ -0,0 +1,12 @@
+<!-- /*
+Annas Nur Cholifah
+203040168
+
+pertemuan 7 == 21 Maret 2021
+Get & Post
+*/ -->
+
+
+<h1>Selamt datang bOZZ</h1>
+
+<a href="login.php"> logout</a>
